72a86ff4416db30fd21fac9f130c8860e9329761
Ebaa <62425957+EbaaCode@users.noreply.github.com>
2024-02-06 15:52:06 +0300

    Added API service and HttpClient

--- angular-client/src/app/api.service.ts
+++ angular-client/src/app/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from './post.interface';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
+// TODO: Add error handling to the API service.
 export class ApiService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  // Using the http client to make a GET request to the API endpoint and fetches the posts.
+  getData(): Observable<Post[]> {
+    return this.http.get<Post[]>(environment.socketUrl + '/api/posts');
+  }
 }

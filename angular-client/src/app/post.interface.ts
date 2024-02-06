f0c72588030505719bcbad5e37ddec82e0a6af7a
Ebaa <62425957+EbaaCode@users.noreply.github.com>
2024-02-06 15:54:01 +0300

    Added post service and Socket.IO

---
+++ angular-client/src/app/post.interface.ts
@@ -0,0 +1,4 @@
+export interface Post {
+    body: string;
+    timestamp: Date;
+}
--- angular-client/src/app/post.service.ts
+++ angular-client/src/app/post.service.ts
@@ -1,9 +1,35 @@
+import { Observable, Observer } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
+import io from 'socket.io-client';
 
+// PostService is a service for sending and receiving posts via a WebSocket connection.
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private socket;
 
-  constructor() { }
+  // Initializes the socket connection.
+  constructor() {
+    this.socket = io(environment.socketUrl);
+  }
+
+  // sendPost is a method that sends a new post to the server via the WebSocket connection.
+  public sendPost(post: string): void {
+    if (this.socket.connected) {
+      this.socket.emit('new-post', post);
+    } else {
+      console.error('Cannot send post, socket is not connected.');
+    }
+  }
+
+  // This Observable emits a new value whenever a new post is received from the server.
+  public getPosts(): Observable<string> {
+    return new Observable((observer: Observer<string>) => {
+      this.socket.on('new-post', (post) => {
+        observer.next(post);
+      });
+    })
+  }
 }
